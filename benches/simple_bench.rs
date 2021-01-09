caee497874616b5860cfd1a45643e84b7a3489d2
Weiyuan Wu <youngw@sfu.ca>
2021-01-09 21:49:33 +0000

    fix benchmark

--- benches/simple_bench.rs
+++ benches/simple_bench.rs
@@ -2,7 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use treerite::{DMatrix, Predictor};
 
 fn simple_bench(c: &mut Criterion) {
-    let model = Predictor::load("examples/iris_model.so", 1).unwrap();
+    let model = Predictor::load("examples/iris.so", 1).unwrap();
     let feat: Vec<f64> = vec![7.7, 2.8, 6.7, 2.];
     c.bench_function("simple", |b| {
         b.iter(|| model.predict_batch::<_, f64>(&DMatrix::from_slice(black_box(&feat)).unwrap(), false, false).unwrap())
